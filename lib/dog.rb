cf4ba64a46fd4e2f653f3e3471bda555ed3b0752
Josh Morris <jmo0624@gmail.com>
2020-01-12 19:20:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,4 +45,59 @@ class Dog
     dog
   end
   
+  def self.find_by_id(id)
+    sql = <<- SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+    
+    DB[:conn].execuite(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.new_from_db(row)
+    attribute_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attribute_hash)
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    sql = <<- SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+    SQL
+    
+    dog = DB[:conn].execute(sql, name, breed).first
+    
+    if dog
+      new_dog = self.new_from_db(dog)
+    else
+      new_dog = sele.create({:name => name, :breed => breed})
+    end
+    new_dog
+  end
+  
+  def self.find_by_name(name)
+    sql = <<- SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      SQL
+      
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def update
+    sql = <<- SQL
+      UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+    
+  end
   
