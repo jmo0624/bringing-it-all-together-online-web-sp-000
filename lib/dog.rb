03d6feb3f0b8f0d2246577b87bbeb59325a10c22
Josh Morris <jmo0624@gmail.com>
2020-01-12 19:04:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,4 +9,40 @@ class Dog
   
   def self.creat_table
     sql = <<- SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+      SQL
       
+      DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<- SQL
+      DROP TABLE dogs
+      SQL
+      
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<- SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+      SQL
+      
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    
+    self
+  end
+  
+  def self.create(hash_of_attributes)
+    dog = self.new(hash_of_attributes)
+    dog.save
+    
+    dog
+  end
+  
+  
